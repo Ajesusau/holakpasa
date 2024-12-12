6559b45016808396af930c172b5a7d19f2bb4c70
Ajesusau <ajesusau@hotmail.com>
2024-12-12 17:47:05 +0100

    persona2

---
+++ ft_printf.c
@@ -0,0 +1,5 @@
+#include <unistd.h>
+#include <stdio.h>
+int main(void){
+    printf("Hola universo!");
+}
--- minishell.c
+++ minishell.c
@@ -6,12 +6,16 @@
 /*   By: anareval <anareval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:27:07 by fragamez          #+#    #+#             */
-/*   Updated: 2024/12/12 17:34:04 by anareval         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:44:32 by anareval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+
+void ft_putstr(char c){
+	write(1,c,1);
+}
 
 int main(void)
 {
